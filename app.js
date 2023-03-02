e80466f55946a23beb046cbad70000d79cdad13c
unknown <youmy028@naver.com>
2023-03-02 20:26:58 +0900

    세팅

---
+++ app.js
@@ -0,0 +1,19 @@
+
+import express from "express";
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get("/about", (req, res) => {
+    res.send("About!");
+});
+
+app.get("/setting", (req, res) => {
+    res.send("Setting!");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
